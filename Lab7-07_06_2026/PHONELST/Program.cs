a17e663ca0057e31ad3114b499ce12b33d7ada5a
SinPi310 <rebel.hero6@gmail.com>
2026-06-13 18:47:28 +0200

    Pierwsze kroki

---
+++ Lab7-07_06_2026/PHONELST/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Data;
+
+int input = int.Parse(Console.ReadLine());
+
+// Input:
+// 2
+// 3
+// 911
+// 97625999
+// 91125426
+// 5
+// 113
+// 12340
+// 123440
+// 12345
+// 98346
+
+for(int i = 0; i < input; i++)
+{
+    int phoneCount = int.Parse(Console.ReadLine());
+    Console.WriteLine(phoneCount);
+}
